d5dafa2c764957611f9a91d13389a76dbe3180ae
Ifiok Ekott <ekottifiok@gmail.com>
2023-03-29 20:48:19 +0100

    commit for 0x01

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+def canUnlockAll(boxes):
+    arr = [x for x in range(1, len(boxes))]
+    found = set()
+    for box in boxes:
+        if len(box) == 0 and boxes.index(box) != len(boxes) -1:
+            return False
+        for item in box:
+            if item in arr:
+                found.add(item)
+                break
+    if len(arr) != len(found):
+        return False
+    return True
+    
