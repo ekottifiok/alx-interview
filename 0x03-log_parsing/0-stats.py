152a73feecb37883bbe1e6c1a135abcde96061dd
Ifiok Ekott <ekottifiok@gmail.com>
2023-04-15 07:21:42 +0100

    fixed a pycodestyle problem

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,7 +10,7 @@ one, the line must be skipped)
 After every 10 lines and/or a keyboard interruption (CTRL + C),
 print these statistics from the beginning:
     Total file size: File size: <total size>
-    where <total size> is the sum of all 
+    where <total size> is the sum of all
     previous <file size> (see input format above)
     Number of lines by status code:
         possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
