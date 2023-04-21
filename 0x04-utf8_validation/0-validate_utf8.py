bd161386d7ee88f94a5154bd178a229dfa1d576b
Ifiok Ekott <ekottifiok@gmail.com>
2023-04-21 02:02:12 +0100

    update on utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -18,7 +18,7 @@ def count_leading_ones(byte: int) -> int:
     return 8
 
 
-def validUTF8(data: List[int]):
+def validUTF8(data: List[int]) -> bool:
     """
     Check that a sequence of byte values follows the UTF-8 encoding
     rules.  Does not check for canonicalization (i.e. overlong encodings
@@ -28,13 +28,13 @@ def validUTF8(data: List[int]):
     if not data or not isinstance(data, List):
         return False
 
-    data = iter(data)
+    data = iter(data)   # type: ignore
     for leading_byte in data:
         leading_ones = count_leading_ones(leading_byte)
         if leading_ones in [1, 7, 8]:
             return False        # Illegal leading byte
         for _ in range(leading_ones - 1):
-            trailing_byte = next(data, None)
+            trailing_byte = next(data, None)  # type: ignore
             if trailing_byte is None or trailing_byte >> 6 != 0b10:
                 return False    # Missing or illegal trailing byte
     return True
