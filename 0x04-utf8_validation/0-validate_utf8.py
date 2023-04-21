8824138bcd5d08c62421e3b49fdad07b75a6414d
Ifiok Ekott <ekottifiok@gmail.com>
2023-04-21 01:52:38 +0100

    update on validate_utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,23 +1,40 @@
-def count_leading_ones(byte):
-        for i in range(8):
-            if byte >> (7 - i) == 0b11111111 >> (7 - i) & ~1:
-                return i
-        return 8
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+from typing import List
 
-def validUTF8(data):
-        """
-        Check that a sequence of byte values follows the UTF-8 encoding
-        rules.  Does not check for canonicalization (i.e. overlong encodings
-        are acceptable).
 
-        """
-        data = iter(data)
-        for leading_byte in data:
-            leading_ones = count_leading_ones(leading_byte)
-            if leading_ones in [1, 7, 8]:
-                return False        # Illegal leading byte
-            for _ in range(leading_ones - 1):
-                trailing_byte = next(data, None)
-                if trailing_byte is None or trailing_byte >> 6 != 0b10:
-                    return False    # Missing or illegal trailing byte
-        return True
+def count_leading_ones(byte: int) -> int:
+    """counts leading ones
+
+    Args:
+        byte (int): _description_
+
+    Returns:
+        int: _description_
+    """
+    for i in range(8):
+        if byte >> (7 - i) == 0b11111111 >> (7 - i) & ~1:
+            return i
+    return 8
+
+
+def validUTF8(data: List[int]):
+    """
+    Check that a sequence of byte values follows the UTF-8 encoding
+    rules.  Does not check for canonicalization (i.e. overlong encodings
+    are acceptable).
+
+    """
+    if not data or not isinstance(data, List):
+        return False
+
+    data = iter(data)
+    for leading_byte in data:
+        leading_ones = count_leading_ones(leading_byte)
+        if leading_ones in [1, 7, 8]:
+            return False        # Illegal leading byte
+        for _ in range(leading_ones - 1):
+            trailing_byte = next(data, None)
+            if trailing_byte is None or trailing_byte >> 6 != 0b10:
+                return False    # Missing or illegal trailing byte
+    return True
