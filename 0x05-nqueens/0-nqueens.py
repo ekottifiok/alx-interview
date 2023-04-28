7f50a1b5119a1ef98f5ac53105ce4926c50eb9be
Ifiok Ekott <ekottifiok@gmail.com>
2023-04-28 19:23:35 +0100

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,113 +1,69 @@
-#!/usr/bin/python3
-"""N queens solution finder module.
-"""
-import sys
+#!/usr/bin/env py
+from sys import argv
 
-
-solutions = []
-"""The list of possible solutions to the N queens problem.
-"""
-n = 0
-"""The size of the chessboard.
-"""
-pos = None
-"""The list of possible positions on the chessboard.
-"""
-
-
-def get_input():
-    """Retrieves and validates this program's argument.
-
-    Returns:
-        int: The size of the chessboard.
-    """
-    global n
-    n = 0
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except Exception:
-        print("N must be a number")
-        sys.exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    return n
-
-
-def is_attacking(pos0, pos1):
-    """Checks if the positions of two queens are in an attacking mode.
+def solveNQueens(size: int):
+    """solves the nqueens problem
 
     Args:
-        pos0 (list or tuple): The first queen's position.
-        pos1 (list or tuple): The second queen's position.
+        n (int): _description_
 
     Returns:
-        bool: True if the queens are in an attacking position else False.
-    """
-    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
-        return True
-    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
+        List[List[int]]: _description_
+    """    
+    column_set = set()
+    posDiag = set()
+    negDiag = set()
+    results = []
+    board = [[0] * size for i in range(size)]
+
+    def backtrack(row):
+        """the backtrack recursive function
+
+        Args:
+            row (_type_): _description_
+        """        
+        if row == size:
+            results.append(
+                [[idx, row.index(1)] for idx, row in enumerate(board)]
+            )
+            return
+
+        for column in range(size):
+            if column in column_set or \
+                (row+column) in posDiag or\
+                    (row-column) in negDiag:
+                continue
+
+            column_set.add(column)
+            posDiag.add(row+column)
+            negDiag.add(row-column)
+            board[row][column] = 1
+
+            backtrack(row+1)
+
+            column_set.remove(column)
+            posDiag.remove(row+column)
+            negDiag.remove(row-column)
+            board[row][column] = 0
+
+    backtrack(0)
+    return results
 
 
-def group_exists(group):
-    """Checks if a group exists in the list of solutions.
-
-    Args:
-        group (list of integers): A group of possible positions.
-
-    Returns:
-        bool: True if it exists, otherwise False.
-    """
-    global solutions
-    for stn in solutions:
-        i = 0
-        for stn_pos in stn:
-            for grp_pos in group:
-                if stn_pos[0] == grp_pos[0] and stn_pos[1] == grp_pos[1]:
-                    i += 1
-        if i == n:
-            return True
-    return False
-
-
-def build_solution(row, group):
-    """Builds a solution for the n queens problem.
-
-    Args:
-        row (int): The current row in the chessboard.
-        group (list of lists of integers): The group of valid positions.
-    """
-    global solutions
-    global n
-    if row == n:
-        tmp0 = group.copy()
-        if not group_exists(tmp0):
-            solutions.append(tmp0)
-    else:
-        for col in range(n):
-            a = (row * n) + col
-            matches = zip(list([pos[a]]) * len(group), group)
-            used_positions = map(lambda x: is_attacking(x[0], x[1]), matches)
-            group.append(pos[a].copy())
-            if not any(used_positions):
-                build_solution(row + 1, group)
-            group.pop(len(group) - 1)
-
-
-def get_solutions():
-    """Gets the solutions for the given chessboard size.
-    """
-    global pos, n
-    pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
-    a = 0
-    group = []
-    build_solution(a, group)
-
-
-n = get_input()
-get_solutions()
-for solution in solutions:
-    print(solution)
+if len(argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+size = argv[1]
+try:
+    size = int(size)
+    if not isinstance(size, int):
+        raise ValueError
+except ValueError:
+    print("N must be a number")
+    exit(1)
+if size < 4:
+    print("N must be at least 4")
+    exit(1)
+else:
+    for item in solveNQueens(size):
+        print(item)
