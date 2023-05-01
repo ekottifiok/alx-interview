052f35f5928e714ffeaed30a6898c45328d2839d
Ifiok Ekott <ekottifiok@gmail.com>
2023-05-01 09:10:56 +0100

    update on 0x03

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
-"""A script for parsing HTTP request logs."""
+#!/usr/bin/env py
+"""A script for parsing HTTP request logs.
+"""
 from re import fullmatch
-from typing import Dict, List
+from typing import Dict
 
 
 def extract_input(input_line: str) -> Dict[str, int]:
@@ -14,47 +15,42 @@ def extract_input(input_line: str) -> Dict[str, int]:
     Returns:
         Dict[str, None]: the data extracted
     """
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
-    )
-    info = {
-        'status_code': '0',
-        'file_size': 0,
-    }
 
-    resp_match = fullmatch(log_fmt, input_line)
-    if resp_match is not None:
-        status_code = resp_match.group('status_code')
-        file_size = int(resp_match.group('file_size'))
-        info['status_code'] = status_code
-        info['file_size'] = file_size
-    return info
+    r = fullmatch(
+        '{}\\-{}{}{}{}\\s*'.format(
+            r'\s*(?P<ip>\S+)\s*',
+            r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+            r'\s*"(?P<request>[^"]*)"\s*',
+            r'\s*(?P<status_code>\S+)',
+            r'\s*(?P<file_size>\d+)'
+        ),
+        input_line)
+    return {
+        'status_code': int(r.group('status_code') if r is not None else 0),
+        'file_size': int(r.group('file_size')) if r is not None else 0
+    }
 
 
 def print_statistics(
         total_file_size: int,
-        status_codes_stats: Dict[str, int]) -> None:
+        status_codes_stats: Dict[int, int]) -> None:
     """Prints the accumulated statistics of the HTTP request log.
 
     Args:
-        total_file_size (_type_): _description_
-        status_codes_stats (_type_): _description_
+        total_file_size (int): _description_
+        status_codes_stats (Dict[int, int]): _description_
     """
     print('File size: {:d}'.format(total_file_size), flush=True)
     for status_code in sorted(status_codes_stats.keys()):
         num = status_codes_stats.get(status_code, 0)
         if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
+            print('{:d}: {:d}'.format(status_code, num), flush=True)
 
 
 def update_metrics(
         line: str,
         total_file_size: int,
-        status_codes_stats: Dict[str, int]) -> int:
+        status_codes_stats: Dict[int, int]) -> int:
     """Updates the metrics from a given HTTP request log.
 
     Args:
@@ -66,9 +62,9 @@ def update_metrics(
         int: The new total file size.
     """
     line_info = extract_input(line)
-    status_code = line_info.get('status_code', '0')
+    status_code = line_info.get('status_code', 0)
     if status_code in status_codes_stats.keys():
-        status_codes_stats['status_code'] += 1
+        status_codes_stats[status_code] += 1
     return total_file_size + line_info['file_size']
 
 
@@ -78,8 +74,8 @@ def run() -> None:
     line_num = 0
     total_file_size = 0
     status_codes_stats = {
-        '200': 0, '301': 0, '400': 0, '401': 0,
-        '403': 0, '404': 0, '405': 0, '500': 0,
+        200: 0, 301: 0, 400: 0, 401: 0,
+        403: 0, 404: 0, 405: 0, 500: 0,
     }
     try:
         while True:
--- 0x03-log_parsing/README.md
+++ 0x03-log_parsing/README.md
@@ -1,9 +1,11 @@
 # 0x03. Log Parsing
 
 | `Algorithm` | `Python` |
- 
+
 ## Requirements
+
 ### General
+
 - Allowed editors: vi, vim, emacs
 - All your files will be interpreted/compiled on Ubuntu 14.04 LTS using python3 (version 3.4.3)
 - All your files should end with a new line
@@ -14,19 +16,22 @@
 - The length of your files will be tested using wc
 
 ## Tasks
-0. Log parsing
+
+### 0. Log parsing
+
 Write a script that reads stdin line by line and computes metrics:
 
 - Input format: `<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>` (if the format is not this one, the line must be skipped)
 - After every 10 lines and/or a keyboard interruption `(CTRL + C)`, print these statistics from the beginning:
-    - Total file size: File size: `<total size>`
-    - where <total size> is the sum of all previous <file size> (see input format above)
-    - Number of lines by status code:
-        - possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-        - if a status code doesn’t appear or is not an integer, don’t print anything for this status code
-        - format: `<status code>: <number>`
-        - status codes should be printed in ascending order
+  - Total file size: File size: `<total size>`
+  - where \<total size> is the sum of all previous \<file size> (see input format above)
+  - Number of lines by status code:
+    - possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+    - if a status code doesn’t appear or is not an integer, don’t print anything for this status code
+    - format: `<status code>: <number>`
+    - status codes should be printed in ascending order
 **Warning**: In this sample, you will have random value - it’s normal to not have the same output as this one.
+
 ```txt
 alexa@ubuntu:~/0x03-log_parsing$ cat 0-generator.py
 #!/usr/bin/python3
@@ -90,8 +95,9 @@ KeyboardInterrupt
 KeyboardInterrupt
 alexa@ubuntu:~/0x03-log_parsing$ txt
 ```
-**Repo:**
 
-* GitHub repository: alx-interview
-* Directory: 0x03-log_parsing
-* File: [0-stats.py](./0-stats.py)
+### Repo
+
+- GitHub repository: alx-interview
+- Directory: 0x03-log_parsing
+- File: [0-stats.py](./0-stats.py)
