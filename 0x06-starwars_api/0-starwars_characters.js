f77381fe6c4c12ee35d572ab8abc7d2e7fabfd24
Ifiok Ekott <ekottifiok@gmail.com>
2023-05-05 04:31:03 +0000

    initial commit for 0x06

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length > 2) {
+    request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (err, _, body) => {
+        if (err) console.log(err);
+        const cName = JSON.parse(body).characters.map(
+            url => new Promise((resolve, reject) => {
+                request(url, (pErr, __, pBody) => {
+                    if (pErr) reject(pErr);
+
+                    resolve(JSON.parse(pBody).name);
+                });
+            }));
+
+        Promise.all(cName)
+            .then(names => console.log(names.join('\n')))
+            .catch(allErr => console.log(allErr));
+    })
+}
