d31a39fc9e8036cbc1085147adb24a6c288d5f3f
Ifiok Ekott <ekottifiok@gmail.com>
2023-05-11 05:05:59 +0000

    implemented a deep copy

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,14 +2,22 @@
 """rotates a matrix"""
 
 
-def rotate_2d_matrix(matrix):
+def rotate_2d_matrix(matrix = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]):
     """rotates a 2d matrix by 90 degrees clockwise
     """
     size = len(matrix)
     inc = [i for i in range(size)]
     dec = [i for i in range(size - 1, -1, -1)]
-    matrix_copy = matrix.copy()
+    matrix_copy = []
+    for i in matrix:
+        temp = []
+        for j in i:
+            temp.append(j)
+        matrix_copy.append(temp)
+
 
     for i in range(size):
         for j, k in zip(inc, dec):
             matrix[i][k] = matrix_copy[j][i]
+
+rotate_2d_matrix()
