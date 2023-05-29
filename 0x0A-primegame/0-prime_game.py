209b7e4b6d08938e6a8b1fc2ba8801fb80a3171d
Ifiok Ekott <ekottifiok@gmail.com>
2023-05-29 15:57:36 +0100

    updated prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,44 @@
-#!/usr/bin/python3
-"""Prime Game
-Maria and Ben are playing a game. Given a set of consecutive integers starting
-1 up to and including n, they take turns choosing a prime number the
-set and removing that number and its multiples the set. The player that
-cannot make a move loses the game.
-"""
-
 
 def isWinner(x, nums):
-    """Prime Game"""
-    if not nums or x < 1:
-        return None
-    n, c, p1 = max(nums), 0, 0
-    aux = [True] * len(range(max(n + 1, 2)))
-    for i in range(2, int(pow(n, 0.5)) + 1):
-        if not aux[i]:
-            continue
-        for j in range(i*i, n + 1, i):
-            aux[j] = False
+    max_n = max(nums)
+    primes = []
+    def calculate_primes():
+        """We can precalculate the primes up to the maximum 'n'
+        that can occur in any round. This will help us
+        optimize the prime number selection during gameplay.
 
-    aux[0] = aux[1] = False
-    for i in range(len(aux)):
-        if aux[i]:
-            c += 1
-        aux[i] = c
+        Args:
+            n (int): _description_
+
+        Returns:
+            list[int]: _description_
+        """
+
+        is_prime = [True] * (max_n + 1)
+        is_prime[0] = is_prime[1] = False
+
+        for i in range(2, int(max_n**.5) + 1):
+            if is_prime[i]:
+                for j in range(i * i, max_n + 1, i):
+                    is_prime[j] = False
+
+        for i in range(2, max_n + 1):
+            if is_prime[i]:
+                primes.append(i)
+
+
+
+    calculate_primes()
+    winners = []
 
     for n in nums:
-        p1 += aux[n] % 2 == 1
+        is_maria_winner = True
 
-    if p1 * 2 == len(nums):
-        return None
+        if n % 2 == 1:
+            count_primes = sum(1 for p in primes if p <= n)
+            if count_primes % 2 == 0:
+                is_maria_winner = False
 
-    if p1 * 2 > len(nums):
-        return "Maria"
-    return "Ben"
+        winners.append("Maria" if is_maria_winner else "Ben")
+    if winners.count('Maria') != winners.count('Ben'):
+        return winners[-1]
